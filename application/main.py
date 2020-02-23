5a7fbdc80419c680d64b9f36ccd4b8376668e0a8
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2020-02-23 18:53:09 +0100

    Added options for opening and closing a video

--- application/main.py
+++ application/main.py
@@ -1,17 +1,58 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
-import torch
-
+from mywindow import Ui_MainWindow
+from PIL import Image
+from PIL.ImageQt import ImageQt, toqpixmap
+from utils import *
+import skvideo.io
+    
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        uic.loadUi("mainwindow.ui", self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.cap = None
+        self.vid_opened = False
+        # uic.loadUi("mainwindow.ui", self)
+        im = Image.open("picasso.jpg")
+        pixImg = toqpixmap(im)
+        self.ui.l_video.setPixmap(pixImg)
+        self.openVideo("a.mp4")
+        self.nextFrame().show()
+    
+    def openVideo(self, vid_path):
+        """Opens the video and stores it's iterator in self.cap
+        
+        Arguments:
+            vid_path {string} -- path to video
+        """
+        self.cap = skvideo.io.FFmpegReader(vid_path)
+        self.vid_opened = True
+    
+    def nextFrame(self):
+        """Returns the next frame in the video as a PIL Image
+        
+        Returns:
+            PIL Image -- The next frame of the video
+        """
+        assert self.vid_opened, ("Calling nextFrame before opening the video")
+        return Image.fromarray(next(self.cap.nextFrame()))
+    
+    def closeVideo(self):
+        """
+        Closes the video if it was opened
+        """
+        if self.vid_opened:
+            self.cap.close()
+        
 
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
+
 window.show()
-app.exec_()
+window.closeVideo()
+sys.exit(app.exec_())
