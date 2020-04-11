b4a7a669ed5f5135b817c14185d4e259210bf410
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2020-04-11 13:44:47 +0200

    Added more comments to cycleVid

--- src/main/python/analyser/Model/Main/cycleVid.py
+++ src/main/python/analyser/Model/Main/cycleVid.py
@@ -14,6 +14,14 @@ class cycleVid:
         self.container = []
 
     def get(self, name):
+        """Finds and returns the video with the given name
+        
+        Arguments:
+            name {str} -- the name of the video
+        
+        Returns:
+            str -- path to the folder where the video is
+        """
         re = None
         logging.info("L: {0}".format(len(self.container)))
         self.idx = 0
@@ -27,18 +35,37 @@ class cycleVid:
         return re
 
     def add(self, name, vid_path):
+        """Adds a new video to the container
+        
+        Arguments:
+            name {str} -- name of the video
+            vid_path {str} -- path to the video
+        """
         self.container.append((name, vid_path))
         print("Add", name)
 
     def current(self):
-        print(len(self.container))
-        print(self.idx)
+        """Returns the path to the currently selected video
+        
+        Returns:
+            str -- path to the currently selected video
+        """
         return self.container[self.idx][1]
     
     def currentType(self):
+        """Returns the name of the currently selected video
+        
+        Returns:
+            str -- name of the currently selected video
+        """
         return self.container[self.idx][0]
     
     def down(self):
+        """Cycle down on the container and return the path to the previous video
+        
+        Returns:
+            str -- path to the previous video
+        """
         if self.idx > 0:
             self.idx -= 1
         else:
@@ -48,6 +75,11 @@ class cycleVid:
         return self.container[self.idx][1]
 
     def up(self):
+        """Cycle up on the container and return the path to the next video
+        
+        Returns:
+            str -- path to the next video
+        """
         if self.idx < len(self.container) - 1:
             self.idx += 1
         else:
