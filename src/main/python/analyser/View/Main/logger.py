63b01c9161e65ed93e7d5ebdc20b243873084952
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2020-04-13 17:00:58 +0200

    Fix for windows permission issue

--- src/main/python/analyser/View/Main/logger.py
+++ src/main/python/analyser/View/Main/logger.py
@@ -3,6 +3,8 @@ from View.Main.loggerUI import Ui_Dialog
 from PyQt5.QtWidgets import QPlainTextEdit
 from PyQt5.QtCore import QObject, pyqtSignal
 import logging
+import os
+import platform
 
 class LoggerText(logging.Handler, QObject):
     appendPlainText = pyqtSignal(str)
@@ -33,7 +35,14 @@ class LogInfo(QDialog, Ui_Dialog):
         logging.getLogger().setLevel(logging.DEBUG)
         self.ui.layout.insertWidget(1, log_handler.widget)
 
-        fh = logging.FileHandler("log_info.log")
+        homedir = os.path.expanduser('~')
+        
+        if platform.system() == 'Windows':
+            datadir = os.sep.join([homedir, 'Application Data', 'Analyser'])
+        else:
+            datadir = os.sep.join([homedir, '.analyser'])
+
+        fh = logging.FileHandler(os.path.join(datadir, "log_info.log"))
         fh.setLevel(logging.DEBUG)
         fh.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(module)s %(funcName)s %(message)s"))
         logging.getLogger().addHandler(fh)
